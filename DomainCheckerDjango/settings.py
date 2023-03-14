183b7f13c248a2d89d03a46d6ad2060b25c5dd39
Ababil Mustax <iemmustaq@gmail.com>
2023-03-15 02:39:24 +0700

    change settings

--- DomainCheckerDjango/settings.py
+++ DomainCheckerDjango/settings.py
@@ -125,7 +125,7 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

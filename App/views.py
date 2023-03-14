600a6be4bf8fe820dcce2443dbb78aa1ddfbf7c6
Ababil Mustax <iemmustaq@gmail.com>
2023-03-15 01:55:29 +0700

    Initial commit

---
+++ App/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+import requests
+import json
+
+
+# Create your views here.
+
+class DomainChecker(View):
+
+    def get(self, request):
+        return render(request, 'App/index.html')
+
+    def post(self, request):
+        if request.method == "POST":
+            con = True
+            url = "https://domain-checker7.p.rapidapi.com/whois"
+            domain = request.POST['domain']
+            querystring = {"domain": domain}
+
+            headers = {
+                "X-RapidAPI-Key": "fe7ac125c5msh94f9c196609b1eep12fb18jsndc6f9e5920c3",
+                "X-RapidAPI-Host": "domain-checker7.p.rapidapi.com"
+            }
+
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            x = json.dumps(response.json())
+            data = json.loads(x)
+            if not data['available']:
+                con = False
+
+            return render(request, 'App/result.html', {
+                'con': con,
+                'available': data['available'],
+                'created_at': data['created_at'],
+                'expires_at': data['expires_at'],
+                'tld': data['tld'],
+                'registrar': data['registrar'],
+                'updated_at': data['updated_at'],
+                'valid': data['valid'],
+                'domain': domain
+            })
